4747494755465c5f427e77e76059188fd4a39af6
Maizied Hasan <mdshuvo40@gmail.com>
2026-01-20 04:31:30 +0600

    fix: match exact repo name casing for github pages base url

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/lorapok_Media_Player/',
+    base: '/Lorapok_Media_Player/',
 })
