34942794dac94a4b2ac145dab588cf2aab2b0a5a
Maizied Hasan <mdshuvo40@gmail.com>
2026-01-20 04:24:27 +0600

    fix: update website base url to match repo name

--- packages/website/vite.config.ts
+++ packages/website/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/lorapok-player/', // Assuming repo name is lorapok-player, adjust if needed
+    base: '/lorapok_Media_Player/',
 })
